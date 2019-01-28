f1c3f914152af6f041c84f4444c9a4efffedac7d
Ryan Lavin <lavinrp@gmail.com>
2019-01-27 23:09:34 -0500

    Initial Setup

---
+++ CMake/FindNana.cmake
@@ -0,0 +1,72 @@
+
+
+set(NANA_FOUND FALSE)
+
+find_path(
+	NANA_INCLUDE_DIR
+	nana/gui.hpp
+	PATH_SUFFIXES
+		include
+	PATHS
+		/usr
+		/usr/local
+		${NANA_DIR}
+		${NANA_ROOT}
+		$ENV{NANA_DIR}
+		$ENV{NANA_ROOT}
+)
+
+find_library(
+	NANA_LIBRARY_RELEASE
+	nana
+	PATH_SUFFIXES
+		Release
+	PATHS
+		/usr
+		/usr/local
+		${NANA_DIR}
+		${NANA_ROOT}
+		$ENV{NANA_DIR}
+		$ENV{NANA_ROOT}
+)
+
+find_library(
+	NANA_LIBRARY_DEBUG
+	nana_d
+	PATH_SUFFIXES
+        Debug
+	PATHS
+		/usr
+		/usr/local
+		${NANA_DIR}
+		${NANA_ROOT}
+		$ENV{NANA_DIR}
+		$ENV{NANA_ROOT}
+)
+
+if(NANA_LIBRARY_RELEASE AND NANA_LIBRARY_DEBUG)
+	set(NANA_LIBRARY debug ${NANA_LIBRARY_DEBUG} optimized ${NANA_LIBRARY_RELEASE})
+endif()
+
+if(NANA_LIBRARY_RELEASE AND NOT NANA_LIBRARY_DEBUG)
+	set(NANA_LIBRARY_DEBUG ${NANA_LIBRARY_RELEASE})
+	set(NANA_LIBRARY ${NANA_LIBRARY_RELEASE})
+endif()
+
+if(NANA_LIBRARY_DEBUG AND NOT NANA_LIBRARY_RELEASE)
+	set(NANA_LIBRARY_RELEASE ${NANA_LIBRARY_DEBUG})
+	set(NANA_LIBRARY ${NANA_LIBRARY_DEBUG})
+endif()
+
+if(NOT NANA_INCLUDE_DIR OR NOT NANA_LIBRARY)
+	if(NANA_FIND_REQUIRED)
+		message(FATAL_ERROR "nana not found.")
+	elseif(NOT NANA_FIND_QUIETLY)
+		message("nana not found.")
+	endif()
+else()
+	set(NANA_FOUND true)
+	if (NOT NANA_FIND_QUIETLY)
+		message(STATUS "nana found: ${NANA_INCLUDE_DIR}")
+	endif()
+endif()
